166ea1aa91c5c4795083403bc906da1c2e125701
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2021-08-21 14:54:36 -0300

    Adding new itens to recycle on conversation tab

--- app/src/main/java/dev/gustavodahora/whatsapp/MainActivity.java
+++ app/src/main/java/dev/gustavodahora/whatsapp/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
